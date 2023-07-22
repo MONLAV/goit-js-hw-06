124c4f9fd8aa25921637d1ca2714c8a54f2a7d78
MONLAV <127471489+MONLAV@users.noreply.github.com>
2023-07-22 13:24:34 +0300

    changes 8 and 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,21 @@
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+const widget = document.querySelector('.widget');
+const colorSpan = widget.querySelector('.color');
+const changeColorButton = widget.querySelector('.change-color');
+changeColorButton.addEventListener('click', function () {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
 
-const textColor = document.querySelector('.color');
-const changeColorBtn = document.querySelector('.change-color');
-const body = document.body;
 
-const changeColor = () => {
-  textColor.textContent = body.style.backgroundColor = getRandomHexColor();
-};
+
+
+
+
+
